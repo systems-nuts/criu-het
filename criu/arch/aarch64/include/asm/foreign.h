71621f3e55006eced3a2b5f2a8db0965c6a2b1c0
abarbala <abarbala@stevens.edu>
2019-06-04 19:03:05 -0400

    added foreign.h includes

---
+++ criu/arch/aarch64/include/asm/foreign.h
@@ -0,0 +1,22 @@
+
+// Antonio Barbalace, Stevens 2019
+
+// to support multiple conversion into a foreign architecture, aarch64 version
+
+
+
+// identity mapping :-)
+static inline int save_task_regs_aarch64(void *x, unsigned long *values) {
+	return 0;
+}
+static inline int arch_alloc_thread_info_aarch64(CoreEntry *core) {
+	return 0;
+}
+	
+static inline void arch_free_thread_info_aarch64(CoreEntry *core){}
+
+
+
+int save_task_regs_x86_64(void *x, unsigned long *values);
+int arch_alloc_thread_info_x86_64(CoreEntry *core);
+void arch_free_thread_info_x86_64(CoreEntry *core);
